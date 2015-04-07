65bab01b55917c018ccc2b8d92a88c0c88845258
James Wetherill <jameswetherill@netscape.net>
2015-04-07 13:19:30 -0400

    Added dictionaries and jwi into the pom file.

--- src/main/java/com/branded/holdings/qpc/service/PhraseProcessorImpl.java
+++ src/main/java/com/branded/holdings/qpc/service/PhraseProcessorImpl.java
@@ -6,10 +6,17 @@
  */
 package com.branded.holdings.qpc.service;
 
-import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.Formatter;
+import java.util.Iterator;
+import java.util.Locale;
+import java.util.TreeSet;
+import java.util.regex.Pattern;
 
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,16 +24,92 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Service;
 
+
+
+import cc.mallet.pipe.CharSequence2TokenSequence;
+import cc.mallet.pipe.CharSequenceLowercase;
+import cc.mallet.pipe.Pipe;
+import cc.mallet.pipe.SerialPipes;
+import cc.mallet.pipe.TokenSequence2FeatureSequence;
+import cc.mallet.pipe.TokenSequenceRemoveStopwords;
+import cc.mallet.pipe.iterator.CsvIterator;
+import cc.mallet.topics.ParallelTopicModel;
+import cc.mallet.topics.TopicInferencer;
+import cc.mallet.types.Alphabet;
+import cc.mallet.types.FeatureSequence;
+import cc.mallet.types.IDSorter;
+import cc.mallet.types.Instance;
+import cc.mallet.types.InstanceList;
+import cc.mallet.types.LabelSequence;
+
 import com.branded.holdings.qpc.model.Phrase;
+
 
 /**
  * 
  */
 @Service
 public class PhraseProcessorImpl implements  PhraseProcessor, ApplicationContextAware {
-	private String stoplists;
+	private File stopListsFile;
+	private InstanceList instances;
+	private int numTopics = 100;
+	
+	
 	@Autowired
-	ApplicationContext ctx;
+	private ApplicationContext ctx;
+	
+	
+	@Override
+	public void setApplicationContext(ApplicationContext arg0)
+			throws BeansException {
+		ctx = arg0;		
+	}
+	
+	/**
+	 * @param numTopics
+	 */
+	public void setNumTopics(int numTopics){
+		this.numTopics = numTopics;
+	}
+	
+	/**
+	 * @param stoplists the stoplists to set
+	 */
+	public void setStoplists(String stoplists) {		
+		getStoplist(stoplists);
+		initializeMalletInstanceList();
+	}
+	
+	private void initializeMalletInstanceList(){
+		ArrayList<Pipe> pipeList = new ArrayList<Pipe>();
+
+		// Pipes: lowercase, tokenize, remove stopwords, map to features
+		pipeList.add( new CharSequenceLowercase() );
+		pipeList.add( new CharSequence2TokenSequence(Pattern.compile("\\p{L}[\\p{L}\\p{P}]+\\p{L}")) );
+		pipeList.add( new TokenSequenceRemoveStopwords(stopListsFile, "UTF-8", false, false, false) );
+		pipeList.add( new TokenSequence2FeatureSequence() );
+
+		instances = new InstanceList (new SerialPipes(pipeList));
+	}
+	
+	private void getStoplist(String stoplists){
+		try {
+			
+			stopListsFile =ctx.getResource("classpath:"+stoplists).getFile();
+//			BufferedReader reader
+//			   = new BufferedReader(new FileReader(in));
+//			StringBuilder lines = new StringBuilder();
+//			String line = null;
+//            while ((line = reader.readLine()) != null) {
+//               lines.append(line);
+//            }
+//			reader.close();		
+//			stoplists = lines.toString();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 	
 	/* (non-Javadoc)
 	 * @see com.branded.holdings.qpc.service.PhraseProcessor#processPhrase(com.branded.holdings.qpc.model.Phrase)
@@ -36,46 +119,90 @@ public class PhraseProcessorImpl implements  PhraseProcessor, ApplicationContext
 		return null;
 	}
 
-	/**
-	 * @return the stoplists
-	 */
-	public String getStoplists() {
-		return stoplists;
-	}
+	
+	
+	private void getTopics(Phrase phrase) throws IOException{
+		StringBuffer buffer = new StringBuffer(phrase.getDescription());
+		
+		
+		Reader fileReader = new InputStreamReader(new ByteArrayInputStream(buffer.toString().getBytes()), "UTF-8");
+		instances.addThruPipe(new CsvIterator (fileReader, Pattern.compile("^(\\S*)[\\s,]*(\\S*)[\\s,]*(.*)$"),
+											   3, 2, 1)); // data, label, name fields
 
-	/**
-	 * @param stoplists the stoplists to set
-	 */
-	public void setStoplists(String stoplists) {
-		this.stoplists = stoplists;
-		getStoplistText();
+		// Create a model with 100 topics, alpha_t = 0.01, beta_w = 0.01
+		//  Note that the first parameter is passed as the sum over topics, while
+		//  the second is 
+		ParallelTopicModel model = new ParallelTopicModel(numTopics, 10.0, 0.1);
+
+		model.addInstances(instances);
+
+		// Use two parallel samplers, which each look at one half the corpus and combine
+		//  statistics after every iteration.
+		model.setNumThreads(2);
+
+		// Run the model for 50 iterations and stop (this is for testing only, 
+		//  for real applications, use 1000 to 2000 iterations)
+		model.setNumIterations(50);
+		model.estimate();
+
+		// Show the words and topics in the first instance
+
+		// The data alphabet maps word IDs to strings
+		Alphabet dataAlphabet = instances.getDataAlphabet();
+		
+		FeatureSequence tokens = (FeatureSequence) model.getData().get(0).instance.getData();
+		LabelSequence topics = model.getData().get(0).topicSequence;
+		
+		Formatter out = new Formatter(new StringBuilder(), Locale.US);
+//		for (int position = 0; position < tokens.getLength(); position++) {
+//			out.format("%s-%d ", dataAlphabet.lookupObject(tokens.getIndexAtPosition(position)), topics.getIndexAtPosition(position));
+//		}
+//		System.out.println(out);
+		
+		// Estimate the topic distribution of the first instance, 
+		//  given the current Gibbs state.
+		double[] topicDistribution = model.getTopicProbabilities(0);
+
+		// Get an array of sorted sets of word ID/count pairs
+		ArrayList<TreeSet<IDSorter>> topicSortedWords = model.getSortedWords();
+		
+		// Show top 5 words in topics with proportions for the first document
+		for (int topic = 0; topic < numTopics; topic++) {
+			Iterator<IDSorter> iterator = topicSortedWords.get(topic).iterator();
+			
+			out = new Formatter(new StringBuilder(), Locale.US);
+			out.format("%d\t%.3f\t", topic, topicDistribution[topic]);
+			int rank = 0;
+			while (iterator.hasNext() && rank < 5) {
+				IDSorter idCountPair = iterator.next();
+				out.format("%s (%.0f) ", dataAlphabet.lookupObject(idCountPair.getID()), idCountPair.getWeight());
+				rank++;
+			}
+		}
+	
+		// Create a new instance with high probability of topic 0
+		StringBuilder topicZeroText = new StringBuilder();
+		Iterator<IDSorter> iterator = topicSortedWords.get(0).iterator();
+
+		int rank = 0;
+		while (iterator.hasNext() && rank < 5) {
+			IDSorter idCountPair = iterator.next();
+			topicZeroText.append(dataAlphabet.lookupObject(idCountPair.getID()) + " ");
+			rank++;
+		}
+
+		// Create a new instance named "test instance" with empty target and source fields.
+		InstanceList testing = new InstanceList(instances.getPipe());
+		testing.addThruPipe(new Instance(topicZeroText.toString(), null, "test instance", null));
+
+		TopicInferencer inferencer = model.getInferencer();
+		double[] testProbabilities = inferencer.getSampledDistribution(testing.get(0), 10, 1, 10);
+		System.out.println("0\t" + testProbabilities[0]);
 	}
 	
-	private void getStoplistText(){
-		try {
-			
-			File in =ctx.getResource("classpath:"+stoplists).getFile();
-			BufferedReader reader
-			   = new BufferedReader(new FileReader(in));
-			StringBuilder lines = new StringBuilder();
-			String line = null;
-            while ((line = reader.readLine()) != null) {
-               lines.append(line);
-            }
-			reader.close();		
-			stoplists = lines.toString();
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+	
+	
 
-	@Override
-	public void setApplicationContext(ApplicationContext arg0)
-			throws BeansException {
-		ctx= arg0;
-		
-	}
 
 
 	
